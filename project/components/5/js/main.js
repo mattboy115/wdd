5ef64030bde46c4c8c4e37aba3191acbd280575a
Matt Lee <matthew_c_lee@yahoo.com>
2016-03-13 21:23:19 -0400

    fffdfs

--- project/components/5/js/main.js
+++ project/components/5/js/main.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('h3').on('click', function() {
         $('h3').not(this).siblings().slideUp(200);
         $(this).siblings().slideToggle(200);
+        $(this).children('img').attr('src','../../images/accordion/minus.png');
     });
 });
