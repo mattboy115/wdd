10890649602c8a45096cc8fc636b42554aa19977
Matt Lee <matthew_c_lee@yahoo.com>
2016-03-17 18:01:55 -0400

    fjksdlfjlsk

--- project/components/5/js/main.js
+++ project/components/5/js/main.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
-    $('h3').on('click', function() {
-        $('h3').not(this).siblings().slideUp(200);
-        $(this).siblings().slideToggle(200);
-        $(this).children('img').attr('src','../../images/accordion/minus.png');
-    });
-});
+// $(document).ready(function() {
+//     $('dt').on('click', function() {
+//         $('dt').not(this).siblings().slideUp(200);
+//         $(this).siblings().slideToggle(200);
+//     });
+// });
